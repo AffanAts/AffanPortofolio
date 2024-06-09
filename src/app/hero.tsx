ddbbc7171a706659f04d1a75993c84208945a086
Affan Haidar Atstsabit <affanhaidar232@gmail.com>
2024-06-10 00:25:39 +0700

    new

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -14,7 +14,7 @@ function Hero() {
             className="mb-4 lg:text-3xl !leading-tight text-3xl"   placeholder=""
             onPointerEnterCapture={() => {}}
             onPointerLeaveCapture={() => {}}          >
-            Selamat Datang di Portofolio Pengembangan Website Saya! TESTxxx
+            Selamat Datang di Portofolio Pengembangan Website Saya! TESTfawfxxx
           </Typography>
           <Typography
             variant="lead"
