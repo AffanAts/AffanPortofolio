e95df7ff157916e54a35854cfda103918950b440
Affan Haidar Atstsabit <affanhaidar232@gmail.com>
2024-06-10 01:54:01 +0700

    new

--- src/app/projects.tsx
+++ src/app/projects.tsx
@@ -8,41 +8,25 @@ const PROJECTS = [
     img: "/image/blog-1.svg",
     title: "Mobile App Development",
     desc: "Mobile app designed to help users discover and explore local restaurants and cuisines.",
-  },
-  { 
-    img: "/image/blog2.svg",
-    title: "Landing Page Development",
-    desc: "Promotional landing page for a  fitness website Summer Campaign. Form development included.",
-  },
-  {
-    img: "/image/blog3.svg",
-    title: "Mobile App Development",
-    desc: "Mobile app designed to help users discover and explore local restaurants and cuisines.",
-  },
-  {
-    img: "/image/blog4.svg",
-    title: "E-commerce development",
-    desc: "Ecommerce website offering  access to the latest and greatest gadgets and accessories.",
+    link: "https://clinic-capstone-app.netlify.app/",
   },
   {
     img: "/image/blog-1.svg",
     title: "Mobile App Development",
     desc: "Mobile app designed to help users discover and explore local restaurants and cuisines.",
+    link: "https://clinic-capstone-app.netlify.app/",
   },
   {
-    img: "/image/blog2.svg",
-    title: "Landing Page Development",
-    desc: "Promotional landing page for a  fitness website Summer Campaign. Form development included.",
-  },
-  {
-    img: "/image/blog3.svg",
+    img: "/image/blog-1.svg",
     title: "Mobile App Development",
     desc: "Mobile app designed to help users discover and explore local restaurants and cuisines.",
+    link: "https://clinic-capstone-app.netlify.app/",
   },
   {
-    img: "/image/blog4.svg",
-    title: "E-commerce development",
-    desc: "Ecommerce website offering  access to the latest and greatest gadgets and accessories.",
+    img: "/image/blog-1.svg",
+    title: "Mobile App Development",
+    desc: "Mobile app designed to help users discover and explore local restaurants and cuisines.",
+    link: "https://clinic-capstone-app.netlify.app/",
   },
 ];
 
@@ -50,16 +34,23 @@ export function Projects() {
   return (
     <section className="py-28 px-8">
       <div className="container mx-auto mb-20 text-center">
-        <Typography variant="h2" color="blue-gray" className="mb-4"   placeholder=""
-            onPointerEnterCapture={() => {}}
-            onPointerLeaveCapture={() => {}}>
+        <Typography
+          variant="h2"
+          color="blue-gray"
+          className="mb-4"
+          placeholder=""
+          onPointerEnterCapture={() => {}}
+          onPointerLeaveCapture={() => {}}
+        >
           My Projects
         </Typography>
         <Typography
           variant="lead"
-          className="mx-auto w-full px-4 font-normal !text-gray-500 lg:w-6/12"   placeholder=""
-            onPointerEnterCapture={() => {}}
-            onPointerLeaveCapture={() => {}}        >
+          className="mx-auto w-full px-4 font-normal !text-gray-500 lg:w-6/12"
+          placeholder=""
+          onPointerEnterCapture={() => {}}
+          onPointerLeaveCapture={() => {}}
+        >
           Whether you have a mobile app idea that needs to come to life or a
           website that requires a facelift, I&apos;m here to turn your digital
           dreams into reality.
--- src/app/skills.tsx
+++ src/app/skills.tsx
@@ -14,42 +14,36 @@ import { SkillCard } from "@/components";
 const SKILLS = [
   { 
     icon: RectangleGroupIcon,
-    title: "Frontend Web Development:",
+    title: "Pengembangan Web Frontend",
     children:
-      "Creating beautiful and functional web experiences is my forte. Using the latest technologies and best practices, I design and build websites that captivate and engage users.",
-  },
-  {
-    icon: FingerPrintIcon,
-    title: "Mobile App Development",
-    children:
-      " I specialize in creating responsive and intuitive mobile apps that work seamlessly across iOS & Android devices. From concept to deployment, I handle every stage of the development process.",
+      "Menciptakan pengalaman web yang indah dan fungsional. Dengan menggunakan teknologi terbaru dan praktik terbaik, saya merancang dan membangun situs web yang melibatkan pengguna.",
   },
   {
     icon: SwatchIcon,
-    title: "Technology Stack",
+    title: "Tumpukan Teknologi",
     children:
-      "I'm well-versed in the industry's most popular frontend technologies, including HTML5, CSS3, JavaScript, and frameworks like React and React Native.",
+      "Saya menguasai teknologi frontend paling populer di industri ini, termasuk HTML5, CSS3, SQL, JavaScript, dan library seperti React.js.",
   },
   {
     icon: HashtagIcon,
-    title: " Web Optimization",
+    title: "Pengembangan Backend",
     children:
-      "Performance matters. I optimize websites and apps for speed, ensuring your users enjoy a fast and responsive experience, which in turn boosts user satisfaction and SEO rankings.",
+      "Saya menggunakan GraphQL sebagai bahasa query dan juga bekerja dengan REST API. Saya memanfaatkan Hasura Cloud ataupun Google Cloud Platform untuk solusi backend yang efisien dan skalabel.",
   },
   {
     icon: EyeIcon,
-    title: "User-Centric Design",
+    title: "Desain Berpusat pada Pengguna",
     children:
-      "My development goes hand-in-hand with an eye for design. I create user interfaces that are not only functional but also aesthetically pleasing, providing a seamless and enjoyable user journey.",
-  },
-  {
-    icon: DocumentTextIcon,
-    title: "Testing and Quality Assurance",
-    children:
-      "I rigorously test and debug applications to guarantee a bug-free and secure environment for users. Your peace of mind is as important to me as the functionality of your project.",
+      "Pengembangan saya berjalan seiring dengan mata untuk desain. Saya menciptakan antarmuka pengguna yang tidak hanya fungsional tetapi juga estetis, memberikan perjalanan pengguna yang mulus dan menyenangkan.",
   },
 ];
 
+ // {
+  //   icon: DocumentTextIcon,
+  //   title: "Testing and Quality Assurance",
+  //   children:
+  //     "I rigorously test and debug applications to guarantee a bug-free and secure environment for users. Your peace of mind is as important to me as the functionality of your project.",
+  // },
 export function Skills() {
   return (
     <section className="px-8">
@@ -62,17 +56,9 @@ export function Skills() {
         <Typography variant="h1" color="blue-gray" className="mb-4"   placeholder=""
             onPointerEnterCapture={() => {}}
             onPointerLeaveCapture={() => {}}>
-          What I do
+          Apa yang saya lakukan
         </Typography>
-        <Typography
-          variant="lead"
-          className="mx-auto w-full !text-gray-500 lg:w-10/12"   placeholder=""
-            onPointerEnterCapture={() => {}}
-            onPointerLeaveCapture={() => {}}        >
-          I&apos;m not just a developer; I&apos;m a digital dreamweaver.
-          Crafting immersive online experiences is not just a job but my
-          calling. Discover below how I can help you.
-        </Typography>
+        
       </div>
       <div className="container mx-auto grid grid-cols-1 gap-y-10 md:grid-cols-2 lg:grid-cols-3">
         {SKILLS.map((props, idx) => (
